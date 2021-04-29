b1d7fdbc77942bcaecf3a02edf47d9bb3315255e
jiwooy <jiwyoo629@gmail.com>
2021-04-29 10:28:39 -0400

    prep time and world space canvas bc github ate it last time i think

---
+++ Three Little Pigs/Assets/Scripts/PreparationTime.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class PreparationTime : MonoBehaviour
+{
+    public int prepTime;
+    private float timeLeft;
+    public TextMeshProUGUI timeText;
+
+    public GameObject[] spawners;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = (float) prepTime;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.S.gameState != GameManager.GameState.playing) return;
+
+        if (timeLeft >= 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timeText.text = (Mathf.Ceil(timeLeft)).ToString();
+            if (timeLeft < 0)
+            {
+                for (int i = 0; i < spawners.Length; i++)
+                {
+                    spawners[i].SetActive(true);
+                }
+            }
+        }
+    }
+
+    public void btn_StartAttack()
+    {
+        timeLeft = 0;
+    }
+}
