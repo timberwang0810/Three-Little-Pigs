c406e87a510dade354afad8d6dbec104120b82af
timberwang0810 <zilinwan@andrew.cmu.edu>
2021-05-02 20:45:19 -0400

    implemented basic settings (little bugs exist but should be mostly functional)

---
+++ Three Little Pigs/Assets/Scripts/GlobalManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalManager : MonoBehaviour
+{
+    public Slider bgmSlider;
+    public Slider sfxSlider;
+    private static float currentBGMVolume = 1.0f;
+    private static float currentSFXVolume = 1.0f;
+
+    // Initial settings
+    private void Start()
+    {
+        if (SoundManager.S) SoundManager.S.AdjustBGMVolume(currentBGMVolume);
+        bgmSlider.value = currentBGMVolume;
+        sfxSlider.value = currentSFXVolume;
+    }
+
+    public void OnBGMVolumeAdjusted()
+    {
+        currentBGMVolume = bgmSlider.value;
+        if (SoundManager.S) SoundManager.S.AdjustBGMVolume(currentBGMVolume);
+    }
+
+    public void OnSFXVolumeAdjusted()
+    {
+        currentSFXVolume = sfxSlider.value;
+        if (SoundManager.S) SoundManager.S.AdjustSFXVolume(currentSFXVolume);
+    }
+}
